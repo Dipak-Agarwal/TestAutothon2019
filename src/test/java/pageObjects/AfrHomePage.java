f8049e8406a8223ffa2387f5eeac0f768f5a59be
Dipak Agarwal <dipak123456@gmail.com>
2021-08-13 19:59:44 +0530

    Revert "Revert "Cucumber with Selenium""
    This reverts commit c9a11adc81919aa8f401c10c7e6322d8f4a78b21.

---
+++ src/test/java/pageObjects/AfrHomePage.java
@@ -0,0 +1,11 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+
+public class AfrHomePage {
+	
+	public By homeScreenLogo=By.xpath("//a[@aria-label='The Australian Financial Review']");
+	public By subscriptionText=By.xpath("//h2[text()='Save 50% for your first 3 months.']");
+		
+}
+
--- src/test/java/utilities/Constants.java
+++
@@ -1,8 +0,0 @@
-package utilities;
-
-public class Constants {
-	public static int SLEEP_SLOW = 10;
-	public static int SLEEP_MED= 4;
-	public static int SLEEP_FAST = 1;
-	
-}
